bbbadbf09f1ad6f43cad4b0ffbf711ae0f9e8970
Ruben Martirosyan <mruben93@gmail.com>
2017-05-21 10:51:29 +0400

    toplabs homework login form with List<users>

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class pass
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.login = new System.Windows.Forms.TextBox();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.enter = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "username";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Location = new System.Drawing.Point(169, 190);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(52, 13);
+            this.Password.TabIndex = 1;
+            this.Password.Text = "password";
+            this.Password.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(256, 134);
+            this.login.Name = "login";
+            this.login.ReadOnly = true;
+            this.login.Size = new System.Drawing.Size(100, 20);
+            this.login.TabIndex = 2;
+            this.login.TextChanged += new System.EventHandler(this.login_TextChanged);
+            // 
+            // passBox
+            // 
+            this.passBox.Location = new System.Drawing.Point(256, 187);
+            this.passBox.Name = "passBox";
+            this.passBox.PasswordChar = '*';
+            this.passBox.ReadOnly = true;
+            this.passBox.Size = new System.Drawing.Size(100, 20);
+            this.passBox.TabIndex = 3;
+            this.passBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(242, 238);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(54, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Agree";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // enter
+            // 
+            this.enter.Location = new System.Drawing.Point(194, 261);
+            this.enter.Name = "enter";
+            this.enter.Size = new System.Drawing.Size(162, 79);
+            this.enter.TabIndex = 5;
+            this.enter.Text = "Login";
+            this.enter.UseVisualStyleBackColor = true;
+            this.enter.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 74);
+            this.label2.MinimumSize = new System.Drawing.Size(8, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Logged?";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(456, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 43);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "userList";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 352);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.enter);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.passBox);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.label1);
+            this.Name = "pass";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.TextBox login;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button enter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class pass : Form
+    {
+      
+        public class NUser
+        {
+            public string name { get; set; }
+            public string pass { get; set; }
+            public NUser(string nm, string ps)
+            {
+                name = nm;
+                pass = ps;
+            }
+        }
+        NUser gago = new NUser("gag", "11");
+        NUser samo = new NUser("sam", "22");
+        NUser mukuch = new NUser("mukuch", "33");
+        NUser hihi = new NUser("Hi", "ha");
+
+        List<NUser> userL = new List<NUser>();
+        public List<NUser> test()
+        {
+            userL.Add(gago);
+            return userL;
+        }
+       
+
+        public pass()
+        {
+            InitializeComponent();
+
+           List<NUser> test()
+            {
+                this.userL.Add(gago);
+                this.userL.Add(samo);
+                this.userL.Add(mukuch);
+                this.userL.Add(hihi);
+                return userL;
+            }
+            test();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Agree(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string userName = login.Text;
+            string password = passBox.Text;
+            bool check = checkBox1.Checked;
+            bool userOk = false;
+            if (check)
+            {
+            
+                foreach (NUser user in userL)
+                {
+                    if (user.name == userName && user.pass == password)
+                    {
+                        userOk = true;                        
+                      break;
+                    }
+
+                }
+                if (userOk == true)
+                {
+                    label2.Text = "successfully logged in, " + userName;
+                    MessageBox.Show("successfully logged in, " + userName);
+                  
+                } else
+                    {
+                        MessageBox.Show("wrong password, " + userName +  " try again.");
+                    }
+               
+                 
+                
+            }else
+            {
+                MessageBox.Show("You Should be agree :/");
+              
+               
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            bool check = checkBox1.Checked;
+            if (check)
+            {
+                this.login.ReadOnly = false;
+                this.passBox.ReadOnly = false;
+            } else
+            {
+                this.login.ReadOnly = true;
+                this.passBox.ReadOnly = true;
+         
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+         
+       
+            string H = " ";
+            
+            foreach (NUser user in userL)
+            {
+            
+                H += " name: " + user.name + " passw: " + user.pass +  " //" ;
+                if (userL.IndexOf(user) == userL.Count )
+                break;
+            }
+            MessageBox.Show(H);
+
+            }
+        }
+    }
+
